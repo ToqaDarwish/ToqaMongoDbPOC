ac5cce59e8b0fdaa7ac92b0965cf4fb6c603b0b8
Toqa Darwish <toqa.darwish@flairstech.com>
2021-04-14 15:19:38 +0200

    Intialization Project

---
+++ samples/TestSite/Services/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCore.Identity.Mongo.Model;
+
+namespace SampleSite.Identity
+{
+    public class ApplicationUser : MongoUser
+    {
+        public int? Age { get; set; }
+    }
+}
---
+++ src/AspNetCore.Identity.Mongo/Model/MongoRole.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Identity.Mongo.Model
+{
+    public class MongoRole : MongoRole<ObjectId>
+    {
+        public MongoRole() : base() { }
+
+        public MongoRole(string name) : base(name) { }
+    }
+
+    public class MongoRole<TKey> : IdentityRole<TKey> where TKey : IEquatable<TKey>
+    {
+        public MongoRole()
+        {
+            Claims = new List<IdentityRoleClaim<string>>();
+        }
+
+        public MongoRole(string name) : this()
+        {
+            Name = name;
+            NormalizedName = name.ToUpperInvariant();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public List<IdentityRoleClaim<string>> Claims { get; set; }
+    }
+}
